6985254a90ce388e1ffbbea7567b25a2c297a181
EvanTsai <dark1988128@gmail.com>
2024-04-29 00:15:48 +0800

    feat: 調整首頁就直接顯示功能1的內容，不用等按連結 & 新增任務目前active的連結呈現紅字

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,31 @@
-import { Link, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { pagesName } from "../../Router";
 import "./index.scss";
 
 function Home() {
+  const navigate = useNavigate();
+
+  function getActiveLinkClass({ isActive }) {
+    return isActive ? "active" : "";
+  }
+
+  useEffect(() => {
+    navigate(pagesName.assignment1);
+  }, [navigate]);
+
   return (
     <>
       <nav className="assignment-nav">
-        <Link to={pagesName.assignment1}>Assignment 1</Link>
-        <Link to={pagesName.assignment2}>Assignment 2</Link>
-        <Link to={pagesName.assignment3}>Assignment 3</Link>
+        <NavLink className={getActiveLinkClass} to={pagesName.assignment1}>
+          Assignment 1
+        </NavLink>
+        <NavLink className={getActiveLinkClass} to={pagesName.assignment2}>
+          Assignment 2
+        </NavLink>
+        <NavLink className={getActiveLinkClass} to={pagesName.assignment3}>
+          Assignment 3
+        </NavLink>
       </nav>
       <div className="assignment-container">
         <Outlet />
